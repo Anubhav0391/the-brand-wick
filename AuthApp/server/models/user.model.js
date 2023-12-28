3f1a828b12b305a039dd4137db1b29965d848974
unknown <anubhavdixit268@gmail.com>
2023-12-28 14:06:48 +0530

    userSchemaModified

--- AuthApp/server/models/user.model.js
+++ AuthApp/server/models/user.model.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   name: {type: String,required:true},
+  userName: {type: String,required:true},
   email: {type: String,required:true},
-  gender: {type: String,required:true},
+  phone: {type: Number,required:true},
   password: {type: String,required:true}
 });
 
--- AuthApp/server/routes/user.routes.js
+++ AuthApp/server/routes/user.routes.js
@@ -6,30 +6,35 @@ const UserModel = require("../models/user.model");
 const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res) => {
-  const { name, email, gender, password} = req.body;
+  const { name, userName, email, phone, password } = req.body;
 
   try {
     bcrypt.hash(password, 5, async (err, hash) => {
       if (err) {
-        res.status(400).send({"err":err})
+        res.status(400).send({ err: err });
       } else {
         const existing = await UserModel.findOne({ email });
         if (existing) {
-          res.status(200).send({ "msg": "Email is already registered. Please try with another email !" });
+          res
+            .status(200)
+            .send({
+              msg: "Email is already registered. Please try with another email !",
+            });
         } else {
           let user = new UserModel({
             name,
+            userName,
             email,
-            gender,
+            phone,
             password: hash,
           });
           await user.save();
-          res.status(200).send({ "msg": "Registered Successfully"});
+          res.status(200).send({ msg: "Registered Successfully" });
         }
       }
     });
   } catch (err) {
-    res.status(400).send({"err":err.message});
+    res.status(400).send({ err: err.message });
   }
 });
 
@@ -38,25 +43,28 @@ userRouter.post("/login", async (req, res) => {
 
   try {
     const user = await UserModel.findOne({ email });
-    
+
     if (user) {
       bcrypt.compare(password, user.password, (_, result) => {
         if (result) {
-          const token = jwt.sign({ userId: user._id, user:user.name }, process.env.key);
+          const token = jwt.sign(
+            { userId: user._id, user: user.name },
+            process.env.key
+          );
           res.status(200).send({
-            "msg": `Login Successfull`,
-            "token": token,
-            "user":user
+            msg: `Login Successfull`,
+            token: token,
+            user: user,
           });
         } else {
-          res.status(200).send({ "msg": "Wrong Password" });
+          res.status(200).send({ msg: "Wrong Password" });
         }
       });
     } else {
-      res.status(200).send({ "msg": `${email} is not registered.` });
+      res.status(200).send({ msg: `${email} is not registered.` });
     }
   } catch (err) {
-    res.status(400).send({ "err": err.message });
+    res.status(400).send({ err: err.message });
   }
 });
 
